adafa447d98fae8c3f868d73b4fc2102a852ec26
Pranav Nair <pp6275@srmist.edu.in>
2025-07-20 13:23:08 +0530

    chore: Setup server.ts

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Server Up and Running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on Port - http://localhost:${PORT}`);
+});
