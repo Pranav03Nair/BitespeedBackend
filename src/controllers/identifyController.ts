1c70638fb4f4d2d71fc60be195ad51c311f1b031
Pranav Nair <pp6275@srmist.edu.in>
2025-07-21 18:48:32 +0530

    chore: Adding contact secondary or Updation of conflicting primaries

--- src/controllers/identifyController.ts
+++ src/controllers/identifyController.ts
@@ -20,6 +20,7 @@ export const IdentifyContact = async (req: Request, res: Response) => {
           { phoneNumber: phoneNumber || undefined },
         ],
       },
+      orderBy: { createdAt: "asc" },
     });
 
     // Deduplicate
@@ -29,14 +30,14 @@ export const IdentifyContact = async (req: Request, res: Response) => {
     });
     const uniquePrimaryContacts = Array.from(uniqueContactsMap.values());
 
-    // If none, Create object
+    // Step 2: If no primaries, Create new entry
     if (uniquePrimaryContacts.length === 0) {
       const newPrimaryContact = await prisma.contact.create({
         data: {
           phoneNumber,
           email,
           linkPrecedence: "primary",
-          linkedId: null, // Primary contact has no linkedId
+          linkedId: null,
         },
       });
 
@@ -50,7 +51,58 @@ export const IdentifyContact = async (req: Request, res: Response) => {
       });
     }
 
-    // Step 2:
+    // Step 3: One or two primaries exist
+    if (uniquePrimaryContacts.length === 1) {
+      const primary = uniquePrimaryContacts[0];
+
+      const allLinkedContacts = await prisma.contact.findMany({
+        where: {
+          OR: [{ id: primary.id }, { linkedId: primary.id }],
+        },
+      });
+
+      // Get all possible emails and phones
+      const knownEmails = new Set(
+        allLinkedContacts.map((c) => c.email).filter(Boolean)
+      );
+      const knownPhones = new Set(
+        allLinkedContacts.map((c) => c.phoneNumber).filter(Boolean)
+      );
+
+      const isNewEmail = email && !knownEmails.has(email);
+      const isNewPhone = phoneNumber && !knownPhones.has(phoneNumber);
+
+      // IMP - Flatten to prevent tree
+      const truePrimaryId = primary.linkedId ?? primary.id;
+
+      if (isNewEmail || isNewPhone) {
+        await prisma.contact.create({
+          data: {
+            email,
+            phoneNumber,
+            linkPrecedence: "secondary",
+            linkedId: truePrimaryId,
+          },
+        });
+      }
+    } else if (uniquePrimaryContacts.length === 2) {
+      const [primary1, primary2] = uniquePrimaryContacts;
+
+      // Demote primary2 (Note - We have already ordered using ascending above)
+      await prisma.contact.update({
+        where: { id: primary2.id },
+        data: {
+          linkPrecedence: "secondary",
+          linkedId: primary1.id,
+        },
+      });
+
+      // Flatten to prevent tree
+      await prisma.contact.updateMany({
+        where: { linkedId: primary2.id },
+        data: { linkedId: primary1.id },
+      });
+    }
 
     return res.status(200).json({
       message: "Matching primary contacts fetched successfully",
