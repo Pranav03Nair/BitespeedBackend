f7cde85040f6d81534c15527b058c3ec94b613ba
Pranav Nair <pp6275@srmist.edu.in>
2025-07-21 18:13:08 +0530

    chore: Find primaries and Create if not found

--- src/controllers/identifyController.ts
+++ src/controllers/identifyController.ts
@@ -11,19 +11,62 @@ export const IdentifyContact = async (req: Request, res: Response) => {
       });
     }
 
-    const matchingContacts = await prisma.contact.findMany({
+    // Step 1: Fetch primary contacts (if any)
+    const matchingPrimaryContacts = await prisma.contact.findMany({
       where: {
+        linkPrecedence: "primary",
         OR: [
-          email ? { email } : undefined,
-          phoneNumber ? { phoneNumber } : undefined,
-        ].filter(Boolean) as any,
+          { email: email || undefined },
+          { phoneNumber: phoneNumber || undefined },
+        ],
       },
     });
 
-    return res
-      .status(200)
-      .json({ message: "Functionality Testing", data: matchingContacts });
-    //
+    // Deduplicate
+    const uniqueContactsMap = new Map();
+    matchingPrimaryContacts.forEach((contact) => {
+      uniqueContactsMap.set(contact.id, contact);
+    });
+    const uniquePrimaryContacts = Array.from(uniqueContactsMap.values());
+
+    // If none, Create object
+    if (uniquePrimaryContacts.length === 0) {
+      const newPrimaryContact = await prisma.contact.create({
+        data: {
+          phoneNumber,
+          email,
+          linkPrecedence: "primary",
+          linkedId: null, // Primary contact has no linkedId
+        },
+      });
+
+      return res.status(200).json({
+        contact: {
+          primaryContatctId: newPrimaryContact.id,
+          emails: [newPrimaryContact.email].filter(Boolean),
+          phoneNumbers: [newPrimaryContact.phoneNumber].filter(Boolean),
+          secondaryContactIds: [],
+        },
+      });
+    }
+
+    // Step 2:
+
+    return res.status(200).json({
+      message: "Matching primary contacts fetched successfully",
+      data: uniquePrimaryContacts,
+    });
+
+    /*
+    {
+      "contact":{
+      "primaryContatctId": number,
+      "emails": string[], // first element being email of primary contact
+      "phoneNumbers": string[], // first element being phoneNumber of primary
+      "secondaryContactIds": number[] // Array of all Contact IDs that are "seco
+      }
+    }
+    */
   } catch (err) {
     console.error(err);
     return res.status(500).json({ error: "Server Error" });
